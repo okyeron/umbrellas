22e638b229ddd3b30dfe5a82667f463811b037be
okyeron <1909661+okyeron@users.noreply.github.com>
2022-06-25 23:20:56 -0500

    fix amidiminder permissions issue, add fix script

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo adduser pi www-data
 sudo adduser www-data audio
 
 sudo chmod 644 install/systemd/*
-sudo cp install/systemd/* /etc/systemd/system
+sudo cp install/systemd/ttymidi* /etc/systemd/system
 
 sudo systemctl enable ttymidi0.service 
 sudo systemctl enable ttymidi1.service 
@@ -26,17 +26,21 @@ sudo chown -R www-data:www-data /var/www/html/umbrellas
 sudo chmod -R 755 /var/www/html/umbrellas/
 sudo cp --remove-destination /home/pi/umbrellas/install/99-com.rules  /etc/udev/rules.d/99-com.rules
 
+cd ~/umbrellas
 git clone https://github.com/okyeron/ttymidi.git
 cd ttymidi
 make
 sudo make install
 
+cd ~/umbrellas
 git clone https://github.com/mzero/amidiminder.git
 cd amidiminder
 make
 sudo dpkg -i build/amidiminder.deb
-sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /etc/amidiminder.rules
-
+sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /home/pi/umbrellas/amidiminder.rules
+sudo chown pi:pi /home/pi/umbrellas/amidiminder.rules
+sudo chmod 775 /home/pi/umbrellas/amidiminder.rules
+sudo cp --remove-destination /home/pi/umbrellas/install/systemd/amidiminder.service /lib/systemd/system/amidiminder.service
 
 sudo raspi-config nonint do_hostname "umbrellas"
 
---
+++ minderfix.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo rm -r ~/umbrellas/ttymidi/amidiminder
+
+cd ~/umbrellas
+git clone https://github.com/mzero/amidiminder.git
+cd amidiminder
+make
+sudo dpkg -i build/amidiminder.deb
+sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /home/pi/umbrellas/amidiminder.rules
+sudo chown pi:pi /home/pi/umbrellas/amidiminder.rules
+sudo chmod 775 /home/pi/umbrellas/amidiminder.rules
+
+sudo cp --remove-destination /home/pi/umbrellas/install/systemd/amidiminder.service /lib/systemd/system/amidiminder.service
