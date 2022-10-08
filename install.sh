425c7353fc382c0b5a0c16344dab026d0acc0e9e
okyeron <1909661+okyeron@users.noreply.github.com>
2022-10-07 21:35:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ make
 sudo dpkg -i build/amidiminder.deb
 sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /home/pi/umbrellas/amidiminder.rules
 sudo chown pi:pi /home/pi/umbrellas/amidiminder.rules
-sudo chmod 775 /home/pi/umbrellas/amidiminder.rules
+sudo chmod 777 /home/pi/umbrellas/amidiminder.rules
 sudo cp --remove-destination /home/pi/umbrellas/install/systemd/amidiminder.service /lib/systemd/system/amidiminder.service
 
 sudo raspi-config nonint do_hostname "umbrellas"
