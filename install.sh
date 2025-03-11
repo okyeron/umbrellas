b0f3d902e9ae16406c312f5c00899c12d4d79062
Steven Noreyko <1909661+okyeron@users.noreply.github.com>
2025-03-10 20:27:11 -0500

    fix for amidiminder

--- install.sh
+++ install.sh
@@ -33,10 +33,9 @@ make
 sudo make install
 
 cd ~/umbrellas
-git clone https://github.com/mzero/amidiminder.git
-cd amidiminder
-make
-sudo dpkg -i build/amidiminder.deb
+wget https://github.com/mzero/amidiminder/releases/download/0.70/amidiminder_0.70_armhf.deb
+sudo dpkg -i amidiminder_0.70_armhf.deb
+
 sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /home/pi/umbrellas/amidiminder.rules
 sudo chown pi:pi /home/pi/umbrellas/amidiminder.rules
 sudo chmod 777 /home/pi/umbrellas/amidiminder.rules
--- minderfix.sh
+++ minderfix.sh
@@ -3,12 +3,14 @@
 sudo rm -r /home/pi/umbrellas/ttymidi/amidiminder
 
 cd ~/umbrellas
-git clone https://github.com/mzero/amidiminder.git
-cd amidiminder
-make
-sudo dpkg -i build/amidiminder.deb
+# git clone https://github.com/mzero/amidiminder.git
+# cd amidiminder
+# make
+# sudo dpkg -i build/amidiminder.deb
+wget https://github.com/mzero/amidiminder/releases/download/0.70/amidiminder_0.70_armhf.deb
+sudo dpkg -i amidiminder_0.70_armhf.deb
+
 sudo cp --remove-destination /home/pi/umbrellas/install/amidiminder.rules  /home/pi/umbrellas/amidiminder.rules
 sudo chown pi:pi /home/pi/umbrellas/amidiminder.rules
 sudo chmod 775 /home/pi/umbrellas/amidiminder.rules
-
 sudo cp --remove-destination /home/pi/umbrellas/install/systemd/amidiminder.service /lib/systemd/system/amidiminder.service
